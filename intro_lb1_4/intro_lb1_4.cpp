915cbfd855aa19c63c962aa047fe74430ad57195
Mykyta Parus <105989712+Nikita-Parus@users.noreply.github.com>
2024-09-28 02:26:17 +0300

    Add files via upload

---
+++ intro_lb1_4/intro_lb1_4.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(0, ".1251");
+	double age, salary;
+	cin >> age >> salary;
+	if (age > 20 && (salary > 400 && salary < 1000)) {
+		cout << "We will consider your position" << endl;
+	}
+	else {
+		cout << "You are not suitable" << endl;
+		return 1;
+	}
+	string name, surname, position;
+	cout << "Iм'я: " << endl;
+	cin >> name;
+	cout << "Прiзвище: " << endl;
+	cin >> surname;
+	cout << "Посада: " << endl;
+	cin >> position;
+	cout << "Ваше iм'я: " << name << " " << surname << endl << "Ваш вiк: " << age << endl << "Ваша посада: " << position << endl << "Зарплата: " << salary;
+	return 0;
+}
